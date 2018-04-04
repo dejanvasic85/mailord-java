4e06ccacec2e6ff613e23033e4354dffac150f3f
Dejan Vasic <dejan@apthletic.com>
2018-04-04 16:10:07 +1000

    Adding test properties for additional logging

--- src/test/java/com/gandalf/EmailTemplateTest.java
+++ src/test/java/com/gandalf/EmailTemplateTest.java
@@ -2,6 +2,7 @@ package com.gandalf;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
 import org.junit.Test;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.GenericType;
@@ -11,6 +12,7 @@ import static org.junit.Assert.assertEquals;
 public class EmailTemplateTest extends JerseyTest {
 
     protected Application configure(){
+        enable(TestProperties.LOG_TRAFFIC);
         return new ResourceConfig().packages("com.gandalf");
     }
 
